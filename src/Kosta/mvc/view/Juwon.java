3effe548a4a0e4b12bd19da3e5ff93eaedfe47d0
KOSTA <KOSTA@192.168.0.183>
2020-09-14 15:59:56 +0900

    Soeun.java수정-소은

--- src/Kosta/mvc/view/Juwon.java
+++ src/Kosta/mvc/view/Juwon.java
@@ -3,5 +3,6 @@ package Kosta.mvc.view;
 public class Juwon {
 	public void test() {
 		System.out.println("1234566789000987543221");
+		System.out.println("馬馬馬馬馬馬");
 	}
 }
--- src/Kosta/mvc/view/SoEun.java
+++ src/Kosta/mvc/view/SoEun.java
@@ -5,5 +5,6 @@ public class SoEun {
 		
 		System.out.println("김소은 테스트");
 		System.out.println("다른 파일 수정하기 테스트");
+		System.out.println("아아아아아아");
 	}
 }
